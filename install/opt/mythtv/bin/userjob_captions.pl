61f3f11c3d414fd7ab6fcda7f29795e9e38e5bf6
Peter Bennett <pgbennett@comcast.net>
2016-04-09 20:10:50 -0400

    Fix userjob_captions.pl for creating external captions.

--- install/opt/mythtv/bin/userjob_captions.pl
+++ install/opt/mythtv/bin/userjob_captions.pl
@@ -5,12 +5,6 @@
 # filename subtitle_type
 # 9999_999999999.mpg 708
 
-
-
-#    print ((shift @ARGV) . "\n")
-#    $a = shift
-#    print $a . "\n"
-
     use MythTV;
 
 # Connect to mythbackend
@@ -18,36 +12,33 @@
 
     my $basename = @ARGV[0];
     my $search = @ARGV[1];
-    print "Name = $basename\n";
     my $sgroup = new MythTV::StorageGroup();
     my $basedir = $sgroup->FindRecordingDir($basename);
-    print "basedir = $basedir\n";
     my $fullfilename = "$basedir/$basename";
-    print "fullfilename = $fullfilename\n";
     system ("ls", "-l", $fullfilename) == 0 or die "File Not Found";
-#    system ("mythccextractor", "-i", $basename) == 0 or die "mythcextrator failed";
+    system ("ionice", "-c3", "-p$$") == 0 or die "ionice failed";;
+    system ("nice", "mythccextractor", "-i", $fullfilename) == 0 
+        or die "mythcextrator failed";
 # Examples of file names of srt files produced - 
 # 2706_20160323190200.708-service-01.und.srt
 # 2706_20160323190200.608-cc1.und.srt
     my $bname;
-    if ($basename =~ /([^\.]*)\./) {
-        print "match $1\n";
-    }
     ($bname) = ($basename =~ /([^\.]*)\./);
-    print "bname = $bname\n";
     my $selectedFile = "";
     $selectedFile = select_file("$basedir/$bname*$search*.srt");
     if ($selectedFile eq "") {
         $selectedFile = select_file("$basedir/$bname*.srt");
     }
-    print "selectedFile: $selectedFile link: $basedir/$bname.srt\n";
-    # hard link
+    if ($selectedFile eq "") {
+        die "Error: no subtitle found";
+    }
+    print "Selected subtitles: $selectedFile link: $basedir/$bname.srt\n";
+#   hard link
     link $selectedFile, "$basedir/$bname.srt";
 
-    # parameter is search string
+# parameter is search string
     sub select_file {
         my $search = shift;
-        print "Search $search\n";
         my $selectedFile = "";
         my $selectedFileSize = 0;
         foreach my $file (glob("$search")) {
@@ -58,8 +49,5 @@
                 $selectedFile = $file;
             }
         }
-        print "selectedFile: $selectedFile\n";
         return $selectedFile;
     }
-    
-    
