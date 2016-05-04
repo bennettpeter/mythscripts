bef08a454fbefde764272ec564ed21de39bdb7d0
Peter Bennett <pgbennett@comcast.net>
2016-05-04 14:43:38 -0400

    Do not stop mysql when suspending (commented it out)

--- install/opt/mythtv/bin/post_sleep.sh
+++ install/opt/mythtv/bin/post_sleep.sh
@@ -2,9 +2,9 @@
 #systemd
 #This is run after sleep, hibernate, etc.
 
-if systemctl is-enabled mysql.service ; then
-    systemctl start mysql.service
-fi
+#if systemctl is-enabled mysql.service ; then
+#    systemctl start mysql.service
+#fi
 if systemctl is-enabled mythtv-backend.service ; then
     systemctl start mythtv-backend.service
 fi
--- install/opt/mythtv/bin/pre_sleep.sh
+++ install/opt/mythtv/bin/pre_sleep.sh
@@ -4,7 +4,7 @@
 
 killall mythfrontend
 systemctl stop mythtv-backend.service
-systemctl stop mysql.service
+# systemctl stop mysql.service
 
 # unmount NFS file systems
 timeout 5s umount -a -l -f -t nfs,nfs4
