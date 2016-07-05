cd2046299005c1f578ab6a5ff15ba5a48a1b50a2
Peter Bennett <pbennett@mythtv.org>
2016-07-05 14:05:13 -0400

    Added Mythtv packaging

---
+++ packaging/mythplugins/lightpackage.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+set -e
+scriptname=`readlink -e "$0"`
+scriptpath=`dirname "$scriptname"`
+#scriptname=`basename "$scriptname" .sh`
+gitpath="$PWD"
+cd $scriptpath
+sourcedir=$1
+subrelease=$2
+if [[ "$sourcedir" == "" ]] ; then
+    echo Before running this the latest version mythtv and plugins 
+    echo must be built and mythtv lightpackage must be done.
+    echo Parameter 1 = install dir
+    echo Parameter 2 = subrelease number
+    exit 2
+fi
+sourcedir=`readlink -f "$sourcedir"`
+if [[ "$subrelease" == "" ]] ; then subrelease=0 ; fi
+packagever=`git -C "$gitpath" describe --dirty|cut -c2-`-$subrelease
+installdir=`git -C "$gitpath" rev-parse --show-toplevel`
+installdir=`dirname "$installdir"`
+arch=`dpkg-architecture -q DEB_TARGET_ARCH`
+codename=`lsb_release -c|cut -f 2`
+mythtvpackagename=mythtv-light_${packagever}_${arch}_$codename
+packagename=mythplugins-light_${packagever}_${arch}_$codename
+echo Package $packagename
+if [[ -d $installdir/$packagename ]] ; then
+    ls -l $installdir
+    echo $installdir/$packagename already exists - run with a subrelease number
+    exit 2
+fi
+if [[ ! -d $installdir/$mythtvpackagename ]] ; then
+    echo $installdir/$mythtvpackagename does not exist.
+    echo Build mythtv package first
+    exit 2
+fi
+rm -rf $installdir/$packagename $installdir/$packagename.deb
+cp -a "$sourcedir/" "$installdir/$packagename/"
+# Remove mythtv files so that only plugin files remain
+cd $installdir/$packagename
+(cd $installdir/$mythtvpackagename ; find . \( -type f -o -type l \) -print0) | xargs -0 rm -f
+rc=0
+while [[ "$rc" == 0 ]] ; do
+    find . -type d -empty -print0 | xargs -0 rmdir -v || rc=$?
+done
+mkdir -p $installdir/$packagename/DEBIAN
+strip -g -v `find $installdir/$packagename/usr/bin/ -type f -executable`
+strip -g -v `find $installdir/$packagename/usr/lib/ -type f -executable`
+cat >$installdir/$packagename/DEBIAN/control <<FINISH
+Package: mythplugins-light
+Version: $packagever
+Section: graphics
+Priority: optional
+Architecture: $arch
+Essential: no
+Installed-Size: `du -B1024 -d0 $installdir/$packagename | cut  -f1`
+Maintainer: Peter Bennett <pgbennett@comcast.net>
+Depends: mythtv-light
+Conflicts: mythtv-common, mythtv-frontend, mythtv-backend
+Homepage: http://www.mythtv.org
+Description: MythTV Plugins Light
+ MythTV plugins for the MythTV Light package. MythTV Light must be installed
+ before this package. The following plugins are included for Raspberry Pi:
+  MythArchive 
+  MythBrowser
+  MythGame
+  MythMusic
+  MythNetvision
+  MythNews
+  MythWeather
+  MythZoneMinder
+ The MythGallery plugin is not included as it requires OpenGL
+FINISH
+
+cd $installdir
+fakeroot dpkg-deb --build $packagename
+echo $PWD
+ls -ld ${packagename}*
---
+++ packaging/mythtv/lightpackage.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -e
+scriptname=`readlink -e "$0"`
+scriptpath=`dirname "$scriptname"`
+#scriptname=`basename "$scriptname" .sh`
+gitpath="$PWD"
+# cd $scriptpath
+sourcedir=$1
+subrelease=$2
+if [[ "$sourcedir" == "" ]] ; then
+    echo Before running this the latest version must be built
+    echo with --prefix=/usr --runprefix=/usr and installed
+    echo Parameter 1 = install dir
+    echo Parameter 2 = subrelease number
+    echo Run this from the git source directory
+    exit 2
+fi
+sourcedir=`readlink -f "$sourcedir"`
+if [[ "$subrelease" == "" ]] ; then subrelease=0 ; fi
+packagever=`git -C "$gitpath" describe --dirty|cut -c2-`-$subrelease
+installdir=`git -C "$gitpath" rev-parse --show-toplevel`
+installdir=`dirname "$installdir"`
+arch=`dpkg-architecture -q DEB_TARGET_ARCH`
+codename=`lsb_release -c|cut -f 2`
+packagename=mythtv-light_${packagever}_${arch}_$codename
+echo Package $packagename
+if [[ -d $installdir/$packagename ]] ; then
+    ls -l $installdir
+    echo $installdir/$packagename already exists - run with a subrelease number
+    exit 2
+fi
+rm -rf $installdir/$packagename $installdir/$packagename.deb
+cp -a "$sourcedir/" "$installdir/$packagename/"
+mkdir -p $installdir/$packagename/DEBIAN
+strip -g `find $installdir/$packagename/usr/bin/ -type f -executable`
+strip -g `find $installdir/$packagename/usr/lib/ -type f -executable -name '*.so*'`
+cat >$installdir/$packagename/DEBIAN/control <<FINISH
+Package: mythtv-light
+Version: $packagever
+Section: graphics
+Priority: optional
+Architecture: $arch
+Essential: no
+Installed-Size: `du -B1024 -d0 $installdir/$packagename | cut  -f1`
+Maintainer: Peter Bennett <pgbennett@comcast.net>
+Depends: libavahi-compat-libdnssd1, libqt5widgets5, libqt5script5, libqt5sql5-mysql, libqt5xml5, libqt5network5, libqt5webkit5, libexiv2-13 | libexiv2-14, pciutils, libva-x11-1, libva-glx1, libqt5opengl5, libdbi-perl,  libdbd-mysql-perl, libnet-upnp-perl, python-lxml, python-mysqldb
+Conflicts: mythtv-common, mythtv-frontend, mythtv-backend
+Homepage: http://www.mythtv.org
+Description: MythTV Light
+ Light weight package that installs MythTV in one package, front end
+ and backend. Does not install database or services.
+FINISH
+
+if [[ "$arch" == armhf && "$codename" == xenial ]] ; then
+    environ="env LD_LIBRARY_PATH=/usr/lib/arm-linux-gnueabihf/mesa-egl "
+else
+    environ=
+fi
+
+mkdir -p $installdir/$packagename/usr/share/applications/
+sed -e "s~@env@~$environ~" < $scriptpath/lightpackage/mythtv.desktop > $installdir/$packagename/usr/share/applications/mythtv.desktop
+sed -e "s~@env@~$environ~" < $scriptpath/lightpackage/mythtv-setup.desktop > $installdir/$packagename/usr/share/applications/mythtv-setup.desktop
+
+mkdir -p $installdir/$packagename/usr/share/pixmaps/
+cp -f $scriptpath/lightpackage/mythtv.png $installdir/$packagename/usr/share/pixmaps/
+
+mkdir -p $installdir/$packagename/usr/share/menu/
+sed -e "s~@env@~$environ~" < $scriptpath/lightpackage/mythtv-frontend > $installdir/$packagename/usr/share/menu/mythtv-frontend
+
+cd $installdir
+fakeroot dpkg-deb --build $packagename
+echo $PWD
+ls -ld ${packagename}*
