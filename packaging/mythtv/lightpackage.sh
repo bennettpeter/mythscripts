5ad675191937933211f060c851f1b8d14f6c8e48
Peter Bennett <pbennett@mythtv.org>
2016-09-04 16:10:51 -0400

    Different deps for ubuntu or jessie

--- packaging/mythtv/lightpackage.sh
+++ packaging/mythtv/lightpackage.sh
@@ -62,6 +62,10 @@ fi
 mkdir -p $installdir/$packagename/DEBIAN
 strip -g `find $installdir/$packagename/usr/bin/ -type f -executable`
 strip -g `find $installdir/$packagename/usr/lib/ -type f -executable -name '*.so*'`
+deps="libtag1v5,"
+if [[ "$arch" == armhf ]] ; then
+    deps="libtag1c2a,"
+fi
 cat >$installdir/$packagename/DEBIAN/control <<FINISH
 Package: mythtv-light
 Version: $packagerel
@@ -71,7 +75,7 @@ Architecture: $arch
 Essential: no
 Installed-Size: `du -B1024 -d0 $installdir/$packagename | cut  -f1`
 Maintainer: Peter Bennett <pbennett@mythtv.org>
-Depends: libavahi-compat-libdnssd1, libqt5widgets5, libqt5script5, libqt5sql5-mysql, libqt5xml5, libqt5network5, libqt5webkit5, libexiv2-13 | libexiv2-14, pciutils, libva-x11-1, libva-glx1, libqt5opengl5, libdbi-perl,  libdbd-mysql-perl, libnet-upnp-perl, python-lxml, python-mysqldb, python-urlgrabber, libcec3, libfftw3-double3, libfftw3-single3, libtag1c2a, libass5, libfftw3-3, libraw1394-11, libiec61883-0, libavc1394-0, fonts-liberation
+Depends: $deps libavahi-compat-libdnssd1, libqt5widgets5, libqt5script5, libqt5sql5-mysql, libqt5xml5, libqt5network5, libqt5webkit5, libexiv2-13 | libexiv2-14, pciutils, libva-x11-1, libva-glx1, libqt5opengl5, libdbi-perl,  libdbd-mysql-perl, libnet-upnp-perl, python-lxml, python-mysqldb, python-urlgrabber, libcec3, libfftw3-double3, libfftw3-single3, libass5, libfftw3-3, libraw1394-11, libiec61883-0, libavc1394-0, fonts-liberation
 Conflicts: mythtv-common, mythtv-frontend, mythtv-backend
 Homepage: http://www.mythtv.org
 Description: MythTV Light
