fa102332a6faa8601a7fa3680af20cdc1fbb9295
Peter Bennett <pbennett@mythtv.org>
2017-03-12 14:52:52 -0400

    Assign unique local IPV6 addresses

--- install/opt/mythtv/bin/10addipaddress.sh
+++ install/opt/mythtv/bin/10addipaddress.sh
@@ -3,11 +3,12 @@
 ifacename="$1"
 action="$2"
 
-if [[ "$CONNECTION_UUID" == 8d8d0532-0b4c-3566-845b-fe9fb4a80d42 \
-   && "$action" == up ]] ; then
-   ip -6 address add fd3d:63f5:6a89::1/64 dev "$ifacename"
-fi
-if [[ "$CONNECTION_UUID" == e45aa478-9957-3537-a87e-7396ce3bf6dc \
-   && "$action" == up ]] ; then
-   ip -6 address add fd3d:63f5:6a89::2/64 dev "$ifacename"
+if [[ "$CONNECTION_UUID" == "" ]] ; then exit ; fi
+
+set -- `grep "$CONNECTION_UUID" /etc/opt/mythtv/ipv6ulas.txt`
+ipadd="$2"
+
+if [[ "$ipadd" != "" ]] ; then
+    echo "Adding IP Address $ipadd"
+   ip -6 address add $ipadd dev "$ifacename"
 fi
