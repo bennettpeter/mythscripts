001b407a6f8918e372969cbfadee4f01e4016931
Peter Bennett <pbennett@mythtv.org>
2017-08-05 12:33:09 -0400

    Fix install was setting execute on source and png files

--- fixpermissions.sh
+++ fixpermissions.sh
@@ -5,6 +5,7 @@ scriptpath=`dirname "$scriptname"`
 
 cd $scriptpath/
 set -e
+shopt -s extglob
 
 find . -type f -not -name '*.sh' -exec chmod 664 {} + 
 chmod 775 `find . -type f -name '*.sh'`
@@ -12,6 +13,6 @@ chmod 775 `find . -type f -name '*.pl'`
 chmod 775 `find . -type f -name '*.py'`
 chmod 775 `find . -type f -name 'tv_grab_*'`
 chmod 775 `find . -type d`
-chmod 775 install/opt/mythtv/bin/*
+chmod 775 install/opt/mythtv/bin/!(*.png|*.source)
 chgrp -R mythtv *
 
