a231b5841dcb734d2a606da1301eac5f948d9184
Peter Bennett <pbennett@mythtv.org>
2017-08-25 11:55:39 -0400

    Make patch process more generic

--- install/opt/mythtv/bin/prepare_source.sh
+++ install/opt/mythtv/bin/prepare_source.sh
@@ -6,10 +6,10 @@ if [[ `arch` == arm* ]] ; then
     mount /srv/ahome || true
     git checkout .
     rm -f $gitbasedir/../testing.patch
-    if [[ -f $gitbasedir/../../abennettpeter/testing.patch ]] ; then
-        cp $gitbasedir/../../abennettpeter/testing.patch $gitbasedir/../
+    if [[ -f $gitbasedir/../patch/testing.patch ]] ; then
+        cp $gitbasedir/../patch/testing.patch $gitbasedir/../
         git apply $gitbasedir/../testing.patch
     fi
 else
-    git diff > $gitbasedir/../testing.patch
+    git diff > $gitbasedir/../patch/testing.patch
 fi
