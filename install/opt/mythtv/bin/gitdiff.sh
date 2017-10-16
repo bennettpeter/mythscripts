a228254adfb1b99916a1dc8d871c78a78b108e3e
Peter Bennett <pbennett@mythtv.org>
2017-10-16 11:20:19 -0400

    Improve build scripts to allow for added files in patch

---
+++ install/opt/mythtv/bin/gitdiff.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+if git status --porcelain | grep "^A" ; then
+    echo "Cannot do this - there are files in cache" >&2
+    echo "Use git reset to clear them" >&2
+    exit 2
+fi
+Tall=.
+. $HOME/.buildrc
+gitbasedir=`git rev-parse --show-toplevel`
+
+sel="$1"
+build=n
+if [[ "$sel" == build ]] ; then
+    build=y
+    sel=all
+fi
+    
+eval files="$(echo '$'T$sel)"
+if [[ "$files" == "" ]] ; then
+    echo Required: all curr or ticket number >&2
+    echo $Tlist >&2
+    exit 2
+fi
+
+shift
+git status >&2
+echo Files to diff: >&2
+echo "$files" >&2
+(cd "$gitbasedir"; git add $files >&2)
+if [[ "$?" != 0 ]] ; then exit 2 ; fi
+git diff --cached --check >&2
+rc=$?
+if [[ "$rc" == 0 ]] ; then
+    git status >&2
+    if [[ "$build" == n ]] ; then
+        echo enter to continue >&2; read xxxx
+    fi    
+    git diff --cached "$@"
+    rc=$?
+    if [[ -t 1 ]] ; then rc=0 ; fi
+fi
+if [[ "$rc" != 0 ]] ; then
+    echo ERROR ERROR ERROR ERROR ERROR >&2
+fi
+git reset >/dev/null
+exit $rc
--- install/opt/mythtv/bin/prepare_source.sh
+++ install/opt/mythtv/bin/prepare_source.sh
@@ -5,12 +5,12 @@ gitbasedir=`git rev-parse --show-toplevel`
 if [[ `arch` == arm* ]] ; then
     mount /srv/ahome || true
     git checkout $gitbasedir
+    git clean
     rm -f $gitbasedir/../testing.patch
     if [[ -s $gitbasedir/../patch/testing.patch ]] ; then
         cp $gitbasedir/../patch/testing.patch $gitbasedir/../
         git apply -v $gitbasedir/../testing.patch
     fi
 else
-    git diff --check
-    git diff > $gitbasedir/../patch/testing.patch
+    gitdiff.sh build > $gitbasedir/../patch/testing.patch
 fi
