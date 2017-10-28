9b0149b2a135d5c49e5b937788734172229424a1
Peter Bennett <pbennett@mythtv.org>
2017-10-28 18:32:09 -0400

    Fix webcam to restart at 3 AM daily.

--- install/opt/mythtv/bin/webcam.sh
+++ install/opt/mythtv/bin/webcam.sh
@@ -13,24 +13,23 @@ mkdir -p ~/webcam/"$tomorrow"
 if ps -ea|grep fswebcam ; then exit ; fi 
 fswebcam -r 640x480 -l 30 --jpeg 80 -b ~/webcam/%F/cam_%F_%T.jpg 
 
-while true ; do
-    # make today and tomorrow directories
-    today=`date +%F`
-    tomorrow=`date -d "tomorrow" +%F`
-    mkdir -p ~/webcam/"$today"
-    mkdir -p ~/webcam/"$tomorrow"
-    # delete files 7 days old
-    oldest=`date -d "4 days ago" "+%F"`
-    cd ~/webcam/
-    for dir in * ; do
-        if [[ -d "$dir" && "$dir" > "2015-01-01" && "$dir" < "$oldest" ]] ; then
-            rm -rf "$dir"
-        fi
-    done 
-    sleep 1d
-    # weekly reboot
-    if [[ `date +%a` == Sat ]] ; then
-        sudo shutdown -r now
+# make today and tomorrow directories
+today=`date +%F`
+tomorrow=`date -d "tomorrow" +%F`
+mkdir -p ~/webcam/"$today"
+mkdir -p ~/webcam/"$tomorrow"
+# delete files 7 days old
+oldest=`date -d "4 days ago" "+%F"`
+cd ~/webcam/
+for dir in * ; do
+    if [[ -d "$dir" && "$dir" > "2015-01-01" && "$dir" < "$oldest" ]] ; then
+        rm -rf "$dir"
     fi
-done
+done 
+nextboot=`date -d "tomorrow 3AM" +%s`
+now=`date +%s`
+let pause=nextboot-now
+sleep $pause
+# daily 3AM reboot
+sudo shutdown -r now
 
