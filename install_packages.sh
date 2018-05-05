35085bf36c0192a7c6604c24dbb861fcc7410caa
Peter Bennett <pbennett@mythtv.org>
2018-05-05 17:06:48 -0400

    Cougar install of ubuntu bionic

--- install_packages.sh
+++ install_packages.sh
@@ -3,14 +3,19 @@
 
 sudo apt install jq mediainfo libiec61883-dev wakeonlan libxml2-utils curl xprintidle sysstat mysql-client figlet python3-natsort gdebi-core dos2unix
 
+sudo apt install git
+
 sudo apt install fswebcam
 
 if [[ `arch` == x86_64 ]] ; then
     wget -q -O - https://www.bunkus.org/gpg-pub-moritzbunkus.txt | sudo apt-key add -
-    sudo apt-get install mkvtoolnix
-
+    # Add this to /etc/apt/sources.list.d/mkvtoolnix.download.list:
+#deb https://mkvtoolnix.download/ubuntu/ bionic main
+#deb-src https://mkvtoolnix.download/ubuntu/ bionic main
     sudo add-apt-repository ppa:stebbins/handbrake-releases
-    sudo apt-get install handbrake-cli
+
+    sudo apt update
+    sudo apt install mkvtoolnix handbrake-cli ffmpeg
 fi
 
 
