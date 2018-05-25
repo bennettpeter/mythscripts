8d10eca8496c312ded5aa8568e7d343dce43b0e4
Peter Bennett <pbennett@mythtv.org>
2018-05-25 19:12:43 -0400

    Add missing config.sh in home/bin

---
+++ install/home/bin/config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# put this in $HOME/bin as install.sh
+scriptpath=$HOME/proj/github.com/MythTV/packaging/deb-light
+set -e
+
+projname=`basename $PWD`
+
+case $projname in
+    android)
+#        rm -vf build64/mythtv/stamp_configure_android
+        ./mythbuild.sh fresh
+        ;;
+    *)
+        "$scriptpath/config.sh" "$@"
+        ;;
+esac
