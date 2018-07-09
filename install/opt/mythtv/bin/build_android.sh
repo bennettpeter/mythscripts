fc0d660c09bb24386c3ae4e9b8d79c2f40f7c01c
Peter Bennett <pbennett@mythtv.org>
2018-07-09 12:52:13 -0400

    build_android: add comment about path needed in bashrc

--- install/opt/mythtv/bin/build_android.sh
+++ install/opt/mythtv/bin/build_android.sh
@@ -15,7 +15,8 @@ if [[ "$projname" == android ]] ; then
         $BUILD_PREPARE
     fi
     popd
-    # add  --no-plugins for non plugin build in future.
+    # these paths are now in .bashrc so that adb etc can be found.
+    # export PATH=$HOME/android/android-sdk-linux/platform-tools:$HOME/android/android-sdk-linux/build-tools/`ls $HOME/android/android-sdk-linux/build-tools/ | tail -1`:$PATH
     ./mythbuild.sh "$@" 2>&1 | tee mythbuild.log
     echo "results in mythbuild.log"
 fi
