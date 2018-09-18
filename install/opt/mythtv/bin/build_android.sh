d56fb1c3a13fa4bfa8a87c58ec1f39c48e9616db
Peter Bennett <pbennett@mythtv.org>
2018-09-18 15:34:25 -0400

    Change default android build to release.

--- install/opt/mythtv/bin/build_android.sh
+++ install/opt/mythtv/bin/build_android.sh
@@ -17,7 +17,7 @@ if [[ "$projname" == android ]] ; then
     popd
     # these paths are now in .bashrc so that adb etc can be found.
     # export PATH=$HOME/android/android-sdk-linux/platform-tools:$HOME/android/android-sdk-linux/build-tools/`ls $HOME/android/android-sdk-linux/build-tools/ | tail -1`:$PATH
-    ./mythbuild.sh "$@" 2>&1 | tee mythbuild.log
+    ./mythbuild.sh "$@" release 2>&1 | tee mythbuild.log
     echo "results in mythbuild.log"
 fi
 
