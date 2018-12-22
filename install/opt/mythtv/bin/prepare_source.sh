839c3219933026c923b209779ea164f50820de9b
Peter Bennett <pbennett@mythtv.org>
2018-12-22 18:54:10 -0500

    Allow for personal patch on each build.

--- install/opt/mythtv/bin/prepare_source.sh
+++ install/opt/mythtv/bin/prepare_source.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 # Prepare source for build
 set -e
+option="$1"
 rc=0
 gitbasedir=`git rev-parse --show-toplevel`
 mkdir -p $gitbasedir/../patch/ 2>/dev/null || true
+project=`basename "$PWD"`
+for file in $gitbasedir/../patch/Peter/${project}_*.patch ; do
+    if [[ -s "$file" ]] ; then
+        echo Reverse $file
+        git apply --reverse $file || true
+    fi
+done
+if [[ "$option" == done ]] ; then
+    exit $rc
+fi
 if [[ `arch` == arm* ]] ; then
     mount /srv/ahome || true
     git checkout $gitbasedir
@@ -13,16 +24,19 @@ if [[ `arch` == arm* ]] ; then
     fi
 else
     gitdiff.sh build > $gitbasedir/../patch/build.patch || rc=$?
-#    for file in $gitbasedir/../patch/Peter/*.patch ; do
-#        echo git apply -v $file
-#        git apply -v $file
-#    done
-    if [[ "$rc" != 0 ]] ; then
-        echo ERROR - type Y to ignore the error. >&2
-        read xxxx
-        if [[ "$xxxx" == Y ]] ; then
-            rc=0
-        fi
+fi
+for file in $gitbasedir/../patch/Peter/${project}_*.patch ; do
+    if [[ -s "$file" ]] ; then
+        echo Apply $file
+        git apply -v $file || rc=$?
+    fi
+done
+if [[ "$rc" != 0 ]] ; then
+    echo ERROR - type Y to ignore the error. >&2
+    read xxxx
+    if [[ "$xxxx" == Y ]] ; then
+        rc=0
     fi
 fi
+
 exit $rc
