42a7ea7ec227ac34eae3efcccfa45add2a710409
Peter Bennett <pbennett@mythtv.org>
2019-01-01 14:55:21 -0500

    build android to cater for BUILD_DONE command

--- install/opt/mythtv/bin/build_android.sh
+++ install/opt/mythtv/bin/build_android.sh
@@ -10,14 +10,25 @@ if [[ -f $HOME/.buildrc ]] ; then
 fi
 
 if [[ "$projname" == android ]] ; then
-    pushd ../../mythtv/mythtv/
-    if which $BUILD_PREPARE ; then
+    if [[ "$BUILD_PREPARE" != "" ]] ; then
+        pushd ../../mythtv/mythtv/
         $BUILD_PREPARE
+        popd
     fi
-    popd
     # these paths are now in .bashrc so that adb etc can be found.
     # export PATH=$HOME/android/android-sdk-linux/platform-tools:$HOME/android/android-sdk-linux/build-tools/`ls $HOME/android/android-sdk-linux/build-tools/ | tail -1`:$PATH
     ./mythbuild.sh "$@" release 2>&1 | tee mythbuild.log
     echo "results in mythbuild.log"
+    if [[ "$BUILD_DONE" != "" ]] ; then
+        pushd ../../mythtv/mythtv/
+        $BUILD_DONE
+        dirty=`git status --porcelain|grep -v "^??"|wc -l`
+        popd
+        if [[ "$dirty" == 0 ]] ; then
+            apkfile=`ls -t *.apk | head -1`
+            newname=`echo $apkfile | sed s/-dirty/-clean/`
+            mv -v "$apkfile" "$newname"
+        fi
+    fi
 fi
 
