db030943ac95c6e5966d438e62630e4bc4be9c1b
Peter Bennett <pbennett@mythtv.org>
2019-10-11 12:39:36 -0400

    proxy: allow for extended power failures or extended network outages.

--- install/opt/mythtv/bin/startproxy.sh
+++ install/opt/mythtv/bin/startproxy.sh
@@ -13,9 +13,18 @@ echo startproxy.sh
 date
 # sleep to make sure time has been set.
 echo waiting for time sync
+count=0
 until timedatectl show | grep "NTPSynchronized=yes" ; do
   echo -n .
   sleep 1
+  let count=count+1
+  # Approx 12 hours
+  if (( count > 43200 )) ; then
+    date
+    echo "Failed for 12 hours to get network time, shutting down now"
+    sudo shutdown -P now
+    exit 2
+  fi
 done
 date
 
@@ -26,10 +35,21 @@ fi
 now=`date +%s`
 if (( nextshutdown < now )) ; then
   let nextshutdown=now+24*60*60-600
-  nextshutdownstr=`date +%H:%M --date=@$nextshutdown`
   echo $nextshutdown > $DATADIR/nextshutdown
 fi
 nextshutdownstr=`date +%H:%M --date=@$nextshutdown`
+# Time switch is left on standard time year round.
+# In winter it powers on at 2am, in summer at 3am.
+# Sanity check in case power is off for 24 hours.
+if [[ "$nextshutdownstr" < 01:30 || "$nextshutdownstr" > 03:30 ]] ; then
+  echo "Bad shutdown time of $nextshutdownstr, setting it to 01:30"
+  nextshutdownstr=01:30
+  nextshutdown=`date --date="01:30"`
+  if (( nextshutdown < now )) ; then
+    nextshutdown=`date --date="tomorrow 01:30"`
+  fi
+  echo $nextshutdown > $DATADIR/nextshutdown
+fi
 sudo shutdown -P $nextshutdownstr
 
 # Daily IP address check
