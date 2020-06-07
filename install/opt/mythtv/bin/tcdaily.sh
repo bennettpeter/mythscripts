407e13cedf2f7a67a0c4904e86d388d878c42622
Peter Bennett <pbennett@mythtv.org>
2020-06-07 13:48:20 -0400

    Fix for mysql

--- install/opt/mythtv/bin/tcdaily.sh
+++ install/opt/mythtv/bin/tcdaily.sh
@@ -124,13 +124,14 @@ for (( stage=0 ; stage<10 ; stage=stage+1 )) ; do
         delay=${TCDELAY[stage]}
         # lastdate=`date --date=${TCDELAY[stage]}' days ago' '+%y%m%d'`
         lastdate=`date --date=$delay' days ago' '+%y%m%d'`
-        if [[ "$linksrun" == N ]] ; then
-            # Fix missing originalairdate on first run shows.
-            echo "update recorded set originalairdate = DATE(convert_tz(starttime,'UCT','SYSTEM')) where originalairdate = '0000-00-00' and previouslyshown = 0;" | \
-                $mysqlcmd
-            "$scriptpath/mythlinks.sh" airdate
-            linksrun=Y
-        fi        
+# Remove this - it fails with mysql
+#        if [[ "$linksrun" == N ]] ; then
+#            # Fix missing originalairdate on first run shows.
+#            echo "update recorded set originalairdate = DATE(convert_tz(starttime,'UCT','SYSTEM')) where originalairdate = '0000-00-00' and previouslyshown = 0;" | \
+#                $mysqlcmd
+#            "$scriptpath/mythlinks.sh" airdate
+#            linksrun=Y
+#        fi
         rm -rf "$exportdir"
         mkdir -p "$exportdir"/done
         cd "$LINKSDIR"/airdate
