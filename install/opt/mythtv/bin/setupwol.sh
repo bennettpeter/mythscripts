a55eab865dba64ff2c4b5ba8c1b268e5c661f091
Peter Bennett <pbennett@mythtv.org>
2024-07-05 15:23:08 -0400

    Fix setupwol to work with names containing spaces

--- install/opt/mythtv/bin/setupwol.sh
+++ install/opt/mythtv/bin/setupwol.sh
@@ -6,10 +6,13 @@ scriptname=`readlink -e "$0"`
 scriptpath=`dirname "$scriptname"`
 set -e
 arr=($(nmcli c show | grep -w ethernet))
-echo device "${arr[0]}"
-if nmcli c show "${arr[0]}" | grep 802-3-ethernet.wake-on-lan: | grep magic ; then
-	echo WOL is already set
+len=${#arr[@]}
+let words=len-3
+device=${arr[@]:0:$words}
+echo device "$device"
+if nmcli c show "$device" | grep 802-3-ethernet.wake-on-lan: | grep magic ; then
+    echo WOL is already set
 else
-	nmcli c modify "${arr[0]}" 802-3-ethernet.wake-on-lan magic
-	nmcli c show "${arr[0]}" | grep 802-3-ethernet.wake-on-lan:
+    nmcli c modify "$device" 802-3-ethernet.wake-on-lan magic
+    nmcli c show "$device" | grep 802-3-ethernet.wake-on-lan:
 fi
