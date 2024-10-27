e3c9f9a9a6ed683f91744a0ad65daeb3b662cdd4
Peter Bennett <pbennett@mythtv.org>
2024-10-27 10:46:52 -0400

    comskip fix for all episodes with same orig air date

--- install/opt/mythtv/bin/comskip_daily.sh
+++ install/opt/mythtv/bin/comskip_daily.sh
@@ -33,7 +33,7 @@ SELECT basename, recorded.chanid, recorded.starttime, recgroup, title, MAX(type=
 FROM recordedmarkup right outer join recorded using (chanid, starttime)
 where recgroup not in ('Deleted','Shorts') and watched = 0 and title = '$stitle'
 group by basename, recorded.chanid, recorded.starttime, recgroup, title, subtitle, originalairdate
-order by if (originalairdate < "1920-01-01", recorded.starttime, originalairdate)
+order by if (originalairdate < "1920-01-01", recorded.starttime, originalairdate), season, episode
 limit 3;
 EOF
         while IFS=$'\t' read -r basename chanid starttime recgroup title done originalairdate subtitle extra ; do
